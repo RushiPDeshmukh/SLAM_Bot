3fc650f3170013e365d55ad8647c34ee0e9a557e
jidnyesha-patil <jpatil@wpi.edu>
2023-01-29 18:48:11 -0500

    Turns Test

--- src/motor_control/motor_control.ino
+++ src/motor_control/motor_control.ino
@@ -6,49 +6,73 @@
 7,8 Digital
 9,10 PWM
 
+
 ------- | DIR | EN
-Motor 1 | 3   | 5
-Motor 2 | 4   | 6
-Motor 3 | 7   | 9
-Motor 4 | 8   | 10
+Motor 1 | 3   | 5   Rear Left 
+Motor 2 | 4   | 6   Rear Right
+Motor 3 | 7   | 9   Front Left
+Motor 4 | 8   | 10  Front Right
 
 */
 
-Motor motor_1(3,5);
-Motor motor_2(4,6);
-Motor motor_3(7,9);
-Motor motor_4(8,10);
+Motor motor_rl(3,5);
+Motor motor_rr(4,6);
+Motor motor_fl(7,9);
+Motor motor_fr(8,10);
 
 
 void setup() {
 }
 
 void loop() {
+  //Front
+  motor_rl.setSpeed(1,100);
+  motor_rr.setSpeed(1,100);
+  motor_fl.setSpeed(1,100);
+  motor_fr.setSpeed(1,100);
+  delay(5000);
+  //stop
+  motor_rl.setSpeed(0,0);
+  motor_rr.setSpeed(0,0);
+  motor_fl.setSpeed(1,0);
+  motor_fr.setSpeed(1,0);
+  delay(5000);
+  //Backward
+  motor_rl.setSpeed(0,100);
+  motor_rr.setSpeed(0,100);
+  motor_fl.setSpeed(0,100);
+  motor_fr.setSpeed(0,100);
+  delay(5000);
+  // 45 deg left
+  motor_rl.setSpeed(1,100);
+  motor_rr.setSpeed(1,0);
+  motor_fl.setSpeed(1,0);
+  motor_fr.setSpeed(1,100);
+  delay(5000);
+  // 45 deg right
+  motor_rl.setSpeed(1,0);
+  motor_rr.setSpeed(1,100);
+  motor_fl.setSpeed(1,100);
+  motor_fr.setSpeed(1,0);
+  delay(5000);
   
-  motor_1.setSpeed(1,200);
-  motor_2.setSpeed(1,200);
-  motor_3.setSpeed(1,200);
-  motor_4.setSpeed(1,200);
-  delay(5000);
-  motor_1.setSpeed(0,200);
-  motor_2.setSpeed(0,200);
-  motor_3.setSpeed(0,200);
-  motor_4.setSpeed(0,200);
-  delay(5000);
+
+
+
   /*
-  motor_2.setSpeed(1,200);
+  motor_rr.setSpeed(1,200);
   delay(5000);
-  motor_2.setSpeed(0,200);
-  delay(5000);
-  
-  motor_3.setSpeed(1,200);
-  delay(5000);
-  motor_3.setSpeed(0,200);
+  motor_rr.setSpeed(0,200);
   delay(5000);
   
-  motor_4.setSpeed(1,200);
+  motor_fl.setSpeed(1,200);
   delay(5000);
-  motor_4.setSpeed(0,200);
+  motor_fl.setSpeed(0,200);
+  delay(5000);
+  
+  motor_fr.setSpeed(1,200);
+  delay(5000);
+  motor_fr.setSpeed(0,200);
   delay(5000);
   
   */
