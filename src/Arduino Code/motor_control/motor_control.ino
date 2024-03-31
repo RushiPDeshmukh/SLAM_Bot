bb138a9133f921c9861d1b900a562acb22dee4b2
Rushikesh Deshmukh <rdeshmukh@wpi.edu>
2024-03-30 21:47:14 -0400

    Update motor_control.ino with new motor pin out

--- src/Arduino Code/motor_control/motor_control.ino
+++ src/Arduino Code/motor_control/motor_control.ino
@@ -13,12 +13,20 @@ Motor 2 | 4   | 6  ~  Rear Right
 Motor 3 | 7   | 9  ~  Front Left
 Motor 4 | 8   | 10 ~  Front Right
 
+
+New Changes
+------- | DIR | EN(~)
+Motor 1 | 3   | 5  ~  Front Right
+Motor 2 | 4   | 6  ~  Front Left
+Motor 3 | 7   | 9  ~  Rear Right
+Motor 4 | 8   | 10 ~  Rear Left
+
 */
 
-Motor motor_rl(3,5);
-Motor motor_rr(4,6);
-Motor motor_fl(7,9);
-Motor motor_fr(8,10);
+Motor motor_rl(8,10);
+Motor motor_rr(7,9);
+Motor motor_fl(4,6);
+Motor motor_fr(3,5);
 Robot car(15,15,motor_rl,motor_rr,motor_fl,motor_fr);
 
 void setup() {
