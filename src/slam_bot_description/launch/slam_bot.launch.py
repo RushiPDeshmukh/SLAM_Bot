fed8cb67cd1d74bf2fdbc65cb4d2c0a4a82bfd2d
Jidnyesha Patil <jpatil@wpi.edu>
2024-09-01 20:37:13 -0400

    Added EKF Configurations

--- src/slam_bot_description/launch/slam_bot.launch.py
+++ src/slam_bot_description/launch/slam_bot.launch.py
@@ -17,7 +17,7 @@ def generate_launch_description():
   pkg_share = FindPackageShare(package='slam_bot_description').find('slam_bot_description') 
   model = os.path.join(pkg_share, 'models/slam_bot.urdf')
   rviz_config_file = os.path.join(pkg_share, 'rviz/urdf_config.rviz')
-
+  robot_localization_file_path = os.path.join(pkg_share, 'config/ekf.yaml') 
   # Subscribe to the joint states of the robot, and publish the 3D pose of each link.
   start_robot_state_publisher_cmd = Node(
     package='robot_state_publisher',
@@ -51,6 +51,14 @@ def generate_launch_description():
     name='visual_odometry'
   )
 
+  # Start robot localization using an Extended Kalman filter
+  start_robot_localization_cmd = Node(
+    package='robot_localization',
+    executable='ekf_node',
+    name='ekf_filter_node',
+    output='screen',
+    parameters=[robot_localization_file_path])
+
   # Create the launch description and populate
   ld = LaunchDescription()
 
@@ -58,6 +66,7 @@ def generate_launch_description():
   # ld.add_action(start_robot_state_publisher_cmd)
   ld.add_action(start_car_controller)
   ld.add_action(start_camera_imu_publisher)
+  #ld.add_action(start_robot_localization_cmd)
   # # ld.add_action(start_rviz_cmd)
   # ld.add_action(start_visual_odometry)
   return ld
